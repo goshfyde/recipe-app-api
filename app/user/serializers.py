e434c9e71f946185621a01554dd96294923f8194
hayden Bush <haydenbush@haydens-Mac-mini.local>
2019-09-13 09:52:21 -0400

    added manage user endpoing

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -9,7 +9,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
-        fields = ['id', 'email', 'name', 'password']
+        fields = ['email', 'name', 'password']
         extra_kwargs = {
             'password': {
                 'write_only': True,
@@ -20,6 +20,17 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """Create a new user with encrypted password and return it"""
         return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update a user, setting the password correctly and return it"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
 
 
 class AuthTokenSerializer(serializers.Serializer):
